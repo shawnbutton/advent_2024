1532e229a3173e0bcd851d52aa54c7878e3d3c23
shawn <shawnbutton@gmail.com>
2024-12-01 18:35:47 -0500

    day 1 part 1 done

--- src/main/kotlin/com/shawnbutton/advent2024/day01/Day01.kt
+++ src/main/kotlin/com/shawnbutton/advent2024/day01/Day01.kt
@@ -1,15 +1,37 @@
 package com.shawnbutton.advent2024.day01
 
 import com.shawnbutton.advent2024.loadFile
+import kotlin.math.abs
 
 fun Int?.isLessThan(other: Int?) =
     this != null && other != null && this < other
 
 
-
 fun main() {
     val lines = loadFile("/day01.txt")
 
-    print("\n")
+    println(doit(lines))
 }
 
+fun inputToIntegers(input: String): Pair<Int, Int> {
+    val (first, second) = input.split("   ")
+    return Pair(first.toInt(), second.toInt())
+}
+
+fun linesToLists(lines: List<String>): Pair<List<Int>, List<Int>> {
+    return lines
+        .map(::inputToIntegers)
+        .unzip()
+}
+
+fun doit(lines: List<String>): Int {
+    val (first, second) = linesToLists(lines)
+    val firstSorted = first.sorted()
+    val secondSorted = second.sorted()
+
+    return firstSorted
+        .zip(secondSorted)
+        .sumOf { (first, second) ->
+            abs(first - second)
+        }
+}
--- src/test/kotlin/com/shawnbutton/advent2024/day01/Day01Test.kt
+++ src/test/kotlin/com/shawnbutton/advent2024/day01/Day01Test.kt
@@ -4,9 +4,36 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day01Test {
+
+    val inputStrings = listOf(
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+    )
+
     @Test
-    fun should_something() {
-        Assertions.assertEquals(true, true)
+    fun should_parse_a_line_into_integer_tuple() {
+        Assertions.assertEquals(Pair(3, 4), inputToIntegers("3   4"))
+    }
+
+    @Test
+    fun should_get_two_lists_from_lines() {
+        val given = listOf(
+            "3   4",
+            "4   3"
+        )
+
+        val lists = linesToLists(given)
+
+        Assertions.assertEquals(Pair(listOf(3, 4), listOf(4, 3)), lists)
+    }
+
+    @Test
+    fun should_do_it() {
+        Assertions.assertEquals(11, doit(inputStrings))
     }
 
 
