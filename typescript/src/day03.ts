23cd5a4fd4e5da3d9fa83cdd7fae5ddbdfff348e
shawn <shawnbutton@gmail.com>
2024-12-03 21:30:05 -0500

    finish day 3

--- typescript/src/day03.ts
+++ typescript/src/day03.ts
@@ -11,7 +11,14 @@ export function getExpressions(testInput: string) {
     return testInput.match(pattern);
 }
 
+export function getExpressionsWithEnablers(testInput: string) {
+    const pattern = /mul\(\d+,\d+\)|do\(\)|don't\(\)/g;
+    return testInput.match(pattern);
+}
+
 export function evaluate(testInput: string) {
+    if (testInput === 'do()') return -1
+    if (testInput === 'don\'t()') return -2
 
     const first = testInput.match(/mul\((\d+),/);
     const firstNumber = first ? parseInt(first[1], 10) : 0;
@@ -27,8 +34,33 @@ export function doit1(testInput: string) {
         .reduce((sum, current) => sum + current, 0)
 }
 
+export function doit2(testInput: string) {
+    const expressions = getExpressionsWithEnablers(testInput)!
+
+    let on = true
+
+    return expressions
+        .map(evaluate)
+        .reduce((sum, current) => {
+            if (current === -1) {
+                on = true
+                return sum
+            }
+
+            if (current === -2) {
+                on = false
+                return sum
+            }
+
+            if (on) return sum + current
+
+            return sum
+        }, 0)
+}
+
 
 const filePath = join(__dirname, 'day03.txt');
 const fileContent = readFileSync(filePath, 'utf-8');
 
 console.log(doit1(fileContent))
+console.log(doit2(fileContent))
--- typescript/test/day03.spec.ts
+++ typescript/test/day03.spec.ts
@@ -1,4 +1,4 @@
-import {doit1, evaluate, getExpressions, someFunction} from '../src/day03';
+import {doit1, doit2, evaluate, getExpressions, getExpressionsWithEnablers} from '../src/day03';
 
 const testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
@@ -20,6 +20,25 @@ describe('Day 03 part 1', () => {
 
     it('should do it', () => {
         expect(doit1(testInput)).toEqual(161)
+    })
+
+    const testInput2 = 'xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))\n'
+
+    it('should find all expressions plus enable/disable', () => {
+        expect(getExpressionsWithEnablers(testInput2))
+            .toEqual([
+                'mul(2,4)',
+                'don\'t()',
+                'mul(5,5)',
+                'mul(11,8)',
+                'do()',
+                'mul(8,5)'
+            ])
+    })
+
+
+    it('should do it 2', () => {
+        expect(doit2(testInput2)).toEqual(48)
     })
 
 
