f60c82588325c90ce1d7117e2bcb4cc74461db0e
shawn <shawnbutton@gmail.com>
2024-12-03 19:15:16 -0500

    finish day 3 part 1

--- typescript/src/day03.ts
+++ typescript/src/day03.ts
@@ -1,3 +1,34 @@
+import {readFileSync} from 'fs';
+import {join} from 'path';
+
+
 export function someFunction(): string {
-    return 'not implemented'
+    return 'expectation'
 }
+
+export function getExpressions(testInput: string) {
+    const pattern = /mul\(\d+,\d+\)/g;
+    return testInput.match(pattern);
+}
+
+export function evaluate(testInput: string) {
+
+    const first = testInput.match(/mul\((\d+),/);
+    const firstNumber = first ? parseInt(first[1], 10) : 0;
+    const second = testInput.match(/,(\d+)\)/);
+    const secondNumber = second ? parseInt(second[1], 10) : 0;
+
+    return firstNumber * secondNumber
+}
+
+export function doit1(testInput: string) {
+    return getExpressions(testInput)!
+        .map(evaluate)
+        .reduce((sum, current) => sum + current, 0)
+}
+
+
+const filePath = join(__dirname, 'day03.txt');
+const fileContent = readFileSync(filePath, 'utf-8');
+
+console.log(doit1(fileContent))
--- typescript/test/day03.spec.ts
+++ typescript/test/day03.spec.ts
@@ -1,9 +1,26 @@
+import {doit1, evaluate, getExpressions, someFunction} from '../src/day03';
 
-import { someFunction } from '../src/day03';
+const testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
-describe('Empty Kata', () => {
-  it('should pass this test', () => {
 
-    expect(someFunction()).toEqual('expectation')
-  })
+describe('Day 03 part 1', () => {
+    it('should find all expressions', () => {
+        expect(getExpressions(testInput))
+            .toEqual([
+                'mul(2,4)',
+                'mul(5,5)',
+                'mul(11,8)',
+                'mul(8,5)'
+            ])
+    })
+
+    it('should evaluate an expression', () => {
+        expect(evaluate('mul(2,4)')).toEqual(8)
+    })
+
+    it('should do it', () => {
+        expect(doit1(testInput)).toEqual(161)
+    })
+
+
 })
