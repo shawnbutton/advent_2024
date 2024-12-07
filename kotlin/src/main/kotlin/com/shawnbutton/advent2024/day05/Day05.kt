e606e584e7c8efa07d2c145097f664290f556c14
shawn <shawnbutton@gmail.com>
2024-12-07 08:37:19 -0500

    ready for day 5

---
+++ kotlin/src/main/kotlin/com/shawnbutton/advent2024/day05/Day05.kt
@@ -0,0 +1,9 @@
+package com.shawnbutton.advent2024.day05
+
+fun doit1(lines: List<String>): Int {
+    return 123
+}
+
+fun doit2(lines: List<String>): Int {
+    return 123
+}
---
+++ kotlin/src/test/kotlin/com/shawnbutton/advent2024/day05/Day05Test.kt
@@ -0,0 +1,56 @@
+package com.shawnbutton.advent2024.day05
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+
+    val inputStrings = listOf(
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47"
+    )
+
+    @Test
+    fun should_something() {
+        Assertions.assertEquals(true, false)
+    }
+
+
+
+    @Test
+    fun doIt1() {
+        Assertions.assertEquals(2, doit1(inputStrings))
+    }
+
+    @Test
+    fun doIt2() {
+        Assertions.assertEquals(4, doit2(inputStrings))
+    }
+
+}
