a83cc427a9b846545c3c2952508bc3b68ab3c1a3
shawn <shawnbutton@gmail.com>
2024-12-07 15:34:08 -0500

    ready for day 6

---
+++ kotlin/src/main/kotlin/com/shawnbutton/advent2024/day06/Day06.kt
@@ -0,0 +1,17 @@
+package com.shawnbutton.advent2024.day06
+
+import com.shawnbutton.advent2024.loadFile
+
+fun main() {
+    println(doit1())
+}
+
+
+fun doit1(): Int {
+    val lines = loadFile("/day06.txt")
+}
+
+fun doit2(lines: List<String>): Int {
+    return 123
+}
+
---
+++ kotlin/src/test/kotlin/com/shawnbutton/advent2024/day06/Day06Test.kt
@@ -0,0 +1,11 @@
+package com.shawnbutton.advent2024.day06
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+
+
+}
