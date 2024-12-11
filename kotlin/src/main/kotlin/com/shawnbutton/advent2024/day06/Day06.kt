cf2204bf0e048eb0aa79be129deae44fa02123e8
shawn <shawnbutton@gmail.com>
2024-12-10 19:23:33 -0500

    day 6 get grid and starting position

--- kotlin/src/main/kotlin/com/shawnbutton/advent2024/day06/Day06.kt
+++ kotlin/src/main/kotlin/com/shawnbutton/advent2024/day06/Day06.kt
@@ -2,13 +2,36 @@ package com.shawnbutton.advent2024.day06
 
 import com.shawnbutton.advent2024.loadFile
 
+
+fun getStartingPos(grid: List<List<String>>): Pair<Int, Int> {
+    for (y in grid.indices) {
+        for (x in grid[y].indices) {
+            if (grid[y][x] == "^") {
+                return Pair(x, y)
+            }
+        }
+    }
+    throw IllegalArgumentException("Starting position not found in the grid")
+}
+
+fun toGrid(strings: List<String>): List<List<String>> {
+    return strings.map { it.toList().map { char -> char.toString() } }
+}
+
+fun countSteps(grid: List<List<String>>): Int {
+    return 0
+}
+
 fun main() {
-    println(doit1())
+    val lines = loadFile("/day06.txt")
+    println(doit1(lines))
+    println(doit2(lines))
 }
 
 
-fun doit1(): Int {
-    val lines = loadFile("/day06.txt")
+fun doit1(lines: List<String>): Int {
+
+    return 999
 }
 
 fun doit2(lines: List<String>): Int {
--- kotlin/src/test/kotlin/com/shawnbutton/advent2024/day06/Day06Test.kt
+++ kotlin/src/test/kotlin/com/shawnbutton/advent2024/day06/Day06Test.kt
@@ -1,11 +1,50 @@
 package com.shawnbutton.advent2024.day06
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day06Test {
 
+
+    val testInput = listOf(
+        "....#.....",
+        ".........#",
+        "..........",
+        "..#.......",
+        ".......#..",
+        "..........",
+        ".#..^.....",
+        "........#.",
+        "#.........",
+        "......#..."
+    )
+
+
+    @Test
+    fun should_load_grid() {
+        val given = listOf(
+            "ABC",
+            "123"
+        )
+
+        assertEquals(listOf(listOf("A", "B", "C"), listOf("1", "2", "3")), toGrid(given))
+    }
+
+    @Test
+    fun should_get_starting_position() {
+        val grid = toGrid(testInput)
+
+        assertEquals(Pair(4,6), getStartingPos(grid))
+    }
+
+
+    @Test
+    fun should_do_test_grid() {
+        val grid = toGrid(testInput)
+
+        countSteps(grid)
+
+
+    }
 
 }
